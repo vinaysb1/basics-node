2736161d4df6027900a87cda7eb0aa4714a2aff7
unknown <vinaykumarbeelagi@gmail.com>
2024-02-21 15:14:27 +0530

    event emit example

--- demo_uppercase.js
+++ demo_uppercase.js
@@ -1,7 +1,12 @@
 var http = require('http');
-var uc = require('upper-case');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(uc.upperCase("Hello World!"));
-  res.end();
-}).listen(8080);
+
+// Use dynamic import instead of require
+import('upper-case').then(uc => {
+  http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(uc.upperCase("Hello World!"));
+    res.end();
+  }).listen(8080);
+}).catch(err => {
+  console.error('Failed to import upper-case module:', err);
+});
